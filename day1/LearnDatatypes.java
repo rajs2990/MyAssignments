13fe52bc2343fe52f83376cdca53203ed987b9a2
rajs2990 <raj.github2990@gmail.com>
2025-03-29 20:07:56 +0530

    Add files via upload
    Day1 programs [data types and print simple line]

---
+++ day1/LearnDatatypes.java
@@ -0,0 +1,12 @@
+package week1.day1;
+
+public class LearnDatatypes {
+
+public static void main(String[] args) {
+
+   //byte age=30;
+	String name="ann joshuana";
+   System.out.println(name);
+	}
+
+}
